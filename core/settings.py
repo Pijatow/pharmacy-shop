ea1fd45dedf3aed05b9828ef46dec2ab7ca9a601
pijatow <mahdimahanjalali@gmail.com>
2025-05-26 17:21:16 +0330

    fix: fix .env file not being loaded in settings.py with load_dotenv

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
