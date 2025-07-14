c8dd33d575387885accaf2896bcd22cb7b9ad68d
pijatow <mahdimahanjalali@gmail.com>
2025-07-14 03:37:38 +0330

    feat: initial implementation of products app - implemented Product, Collection, Brand and Comment models

---
+++ products/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+from accounts.models import CustomUser
+
+from taggit.managers import TaggableManager
+
+
+class Brand(models.Model):
+    creator = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, related_name="products"
+    )
+    name = models.CharField(max_length=200, unique=True, blank=False)
+    # pictures = models.ImageField()
+    description = models.TextField(max_length=1000, blank=True)
+
+
+def get_unknown_brand():
+    return Brand.objects.get_or_create(name="Unknown", description="Unavailable")
+
+
+class Product(models.Model):
+    creator = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, related_name="products"
+    )
+    name = models.CharField(max_length=200, unique=True, blank=False)
+    brand = models.ForeignKey(
+        on_delete=models.SET_DEFAULT(),
+        default=get_unknown_brand(),
+        related_name="products",
+    )
+    # pictures = models.ImageField()
+    description = models.TextField(max_length=1000, blank=True)
+    # Toman, NOT Rial
+    price = models.IntegerField(help_text="Price stored in Tomans")
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated_at = models.DateTimeField(auto_now=True)
+    tags = TaggableManager(blank=True)
+
+
+class Collection(models.Model):
+    creator = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, related_name="products"
+    )
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    products = models.ManyToManyField(Product, related_name="products")
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, related_name="comments"
+    )
+    content = models.TextField(max_length=1000)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
