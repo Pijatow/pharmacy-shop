246ccbfffbb6a01b7c2007163b214261f00ed9e6
pijatow <mahdimahanjalali@gmail.com>
2025-07-19 08:45:08 +0330

    chore: register product app models in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from products.models import Product, Brand, Comment, Collection
 
-# Register your models here.
+
+admin.site.register([Brand, Collection, Product, Comment])
