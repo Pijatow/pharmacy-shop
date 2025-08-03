4edd1bc76b85c71ebcfed33a6d3dff42d5f7a55c
pijatow <mahdimahanjalali@gmail.com>
2025-08-03 12:53:46 +0330

    feat: implement basic viewsets for Product, Brand, Collection and Comment

--- products/views.py
+++ products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Brand, Product, Collection, Comment
+from .serializers import ProductSerializer, CommentSerializer, CollectionSerializer, BrandSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class BrandViewSet(ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
