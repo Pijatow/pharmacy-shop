3fed52a1095545cca693d9411720a0fb25fde7b2
pijatow <mahdimahanjalali@gmail.com>
2025-10-21 05:20:10 +0330

    feat: update ProductAdmin to set creator on new products and improve field organization

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from rangefilter.filters import NumericRangeFilter, DateRangeQuickSelectListFilter
+
 from products.models import Product, Brand, Comment, Collection
-
-
-from django.contrib import admin
-from .models import Brand, Product, Collection, Comment
+from accounts.models import CustomUser
 
 
 class CommentInline(admin.TabularInline):
@@ -44,20 +46,31 @@ class BrandAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "brand", "price", "creator", "created_at")
-    list_filter = ("brand", "tags", "created_at")
+
+    list_display = ("id", "name", "brand", "price")
+    list_editable = ["price"]
+    list_display_links = ["id", "name"]
+    list_filter = ("brand", ("price", NumericRangeFilter), "tags")
     search_fields = ("name", "brand__name", "description")
     # Use raw_id_fields for better performance with many users or brands.
-    raw_id_fields = ("creator", "brand")
-    readonly_fields = ("created_at", "last_updated_at")
+    raw_id_fields = ("brand",)
+    readonly_fields = ("id", "creator", "created_at", "last_updated_at")
     # Embed the Comment editor into the Product page.
     inlines = [CommentInline]
 
     fieldsets = (
-        ("Core Information", {"fields": ("name", "brand", "description", "picture", "tags")}),
-        ("Pricing and Creator", {"fields": ("price", "creator")}),
+        (
+            "Information",
+            {"fields": ("brand", "name", "description", "price", "tags", "picture")},
+        ),
         ("Timestamps", {"fields": ("created_at", "last_updated_at")}),
     )
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            user = CustomUser.objects.get(id=request.user.id)
+            obj.creator = user
+        obj.save()
 
 
 @admin.register(Collection)
@@ -91,6 +104,7 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = ["author", "text", "created_at", "product"]
     list_display = ("product", "author", "text", "created_at")
     list_filter = ("author",)
     search_fields = ("text", "product__name", "author__username", "author__email")
