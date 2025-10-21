6589345202495f7fae7f591eab011af4cd546bee
pijatow <mahdimahanjalali@gmail.com>
2025-10-21 14:32:45 +0330

    fix: put DOMAIN_NAME env var in allowed_hosts production settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -3,7 +3,7 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [os.getenv("DOMAIN_NAME")]
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 CORS_ALLOW_CREDENTIALS = True
 
